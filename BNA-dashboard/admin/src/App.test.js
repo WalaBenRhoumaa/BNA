76d8f89e4a5f3531d8b7f9d7c41aaa0208b6f109
waladevops <154668132+waladevops@users.noreply.github.com>
2024-06-26 12:32:12 +0200

    App.test.js added

---
+++ BNA-dashboard/admin/src/App.test.js
@@ -0,0 +1,10 @@
+// File: src/components/App.test.js
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders App component', () => {
+  render(<App />);
+  // You can add assertions here to verify the rendered output
+});
